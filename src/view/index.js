90d9a3691c05cea546a8e2505580dfbd408f1c3c
Aleksei Khabirov <finko85@gmail.com>
2021-10-25 00:01:33 +0300

    feat: add render posts

--- src/view/index.js
+++ src/view/index.js
@@ -1,8 +1,15 @@
 import renderForm from './renderForm';
+import renderPosts from './renderPosts';
 
 const render = (elements, watchedState, path, value, prevValue) => {
+  console.log({path})
   if (path.includes('form')) {
     renderForm(elements, watchedState, path, value, prevValue);
+    return;
+  }
+
+  if (path.includes('posts')) {
+    renderPosts(elements, watchedState, path, value, prevValue);
     return;
   }
 };
---
+++ src/view/renderPosts.js
@@ -0,0 +1,45 @@
+const renderPost = (post) => (`
+   <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+     <a
+       href=${post.link}
+       class="fw-bold"
+       data-id="2"
+       target="_blank"
+       rel="noopener noreferrer"
+     >
+      ${post.title}
+     </a>
+     <button 
+       type="button"
+       class="btn btn-outline-primary btn-sm"
+       data-id="2"
+       data-bs-toggle="modal"
+       data-bs-target="#modal"
+     >
+       Просмотр
+     </button>
+   </li>
+`);
+
+const renderPosts = (postsData) => {
+  const posts = postsData.map(renderPost);
+
+  return (`
+    <div class="card border-0">
+       <div class="card-body">
+          <h2 class="card-title h4">Посты</h2>
+       </div>
+       <ul class="list-group border-0 rounded-0">
+        ${posts.join('\n')}
+       </ul>
+    </div>
+  `);
+};
+
+const render = (elements, watchedState, path, value, prevValue) => {
+  const postsMarkup = renderPosts(watchedState.posts);
+  console.log(elements.posts);
+  elements.posts.insertAdjacentHTML('beforeend', postsMarkup);
+};
+
+export default render;
