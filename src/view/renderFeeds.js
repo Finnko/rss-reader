f187b1e5371196fa46c24b1017b7db273e024386
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-11-19 22:24:50 +0300

    fix: lint

--- src/view/renderFeeds.js
+++ src/view/renderFeeds.js
@@ -1,14 +1,14 @@
 import { clearDomNode } from '../util';
 
-const renderFeed = (feed) => (`
+const createFeed = (feed) => (`
   <li class="list-group-item border-0 border-end-0">
     <h3 class="h6 m-0">${feed.title}</h3>
     <p class="m-0 small text-black-50">${feed.desc}</p>
   </li>
 `);
 
-const renderFeeds = (feedsData) => {
-  const feeds = feedsData.map(renderFeed);
+const createFeeds = (feedsData) => {
+  const feeds = feedsData.map(createFeed);
 
   return (`
     <div class="card border-0">
@@ -23,7 +23,7 @@ const renderFeeds = (feedsData) => {
 };
 
 const render = (elements, watchedState) => {
-  const feedsMarkup = renderFeeds(watchedState.feeds);
+  const feedsMarkup = createFeeds(watchedState.feeds);
   clearDomNode(elements.feeds);
   elements.feeds.insertAdjacentHTML('beforeend', feedsMarkup);
 };
