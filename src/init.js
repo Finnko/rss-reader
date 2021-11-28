276611dca1d2390668b28ce8735a77b1dc48171b
Aleksei Khabirov <finko85@gmail.com>
2021-11-28 14:25:47 +0300

    fix: lint

--- src/init.js
+++ src/init.js
@@ -1,20 +1,21 @@
 // @ts-check
-import 'regenerator-runtime/runtime.js';
 import 'bootstrap';
 import i18next from 'i18next';
 import resources from './locales';
 import app from './app.js';
 
-export default async () => {
+export default () => {
   const defaultLanguage = 'ru';
 
   const i18nextInstance = i18next.createInstance();
-  await i18nextInstance.init({
+  const promise = i18nextInstance.init({
     lng: defaultLanguage,
     resources: {
       ru: resources.ru,
     },
   });
 
-  app(i18nextInstance);
+  promise.then(() => {
+    app(i18nextInstance);
+  });
 };
